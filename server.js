320face25b83824a83081acb42422883b9ed6ce8
MatheusAndradeMelo <mandrademelo77@gmail.com>
2022-07-24 00:17:50 -0300

    indentation

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const http = require('http');
+const http = require("http");
 const port = 3000; // default port
 
 const rotas = {
-    '/' : 'Curso de Node',
-    '/livros' : 'Entrei na pag de livros',
-    '/autores' : 'Listagem de autores',
-    '/editora' : 'Pag de editora',
-    '/sobre': 'Info sobre o projeto'
-}
+  "/": "Curso de Node",
+  "/livros": "Entrei na pag de livros",
+  "/autores": "Listagem de autores",
+  "/editora": "Pag de editora",
+  "/sobre": "Info sobre o projeto",
+};
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(rotas[req.url]);
 });
 
 server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
