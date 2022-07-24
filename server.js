f5791d5295cd92fcd1e8c2ca2098510107f0bdb0
MatheusAndradeMelo <mandrademelo77@gmail.com>
2022-07-24 00:14:31 -0300

    add library nodemon, add new rotes, and add a gitignore file

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require('http');
 const port = 3000; // default port
 
+const rotas = {
+    '/' : 'Curso de Node',
+    '/livros' : 'Entrei na pag de livros',
+    '/autores' : 'Listagem de autores',
+    '/editora' : 'Pag de editora',
+    '/sobre': 'Info sobre o projeto'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Curso APi NodeJs!');
+    res.end(rotas[req.url]);
 });
 
 server.listen(port, () => {
